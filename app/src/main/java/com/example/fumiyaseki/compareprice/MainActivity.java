e543c54006b29c3eefe018a57cf180f21045ce9c
mac Fumiya <fumipumisecky@gmail.com>
2016-03-03 19:57:34 +0900

    get webViews

--- app/src/main/java/com/example/fumiyaseki/compareprice/MainActivity.java
+++ app/src/main/java/com/example/fumiyaseki/compareprice/MainActivity.java
@@ -8,45 +8,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView1, webView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        webView1 = (WebView)findViewById(R.id.webView1);
+        webView2 = (WebView)findViewById(R.id.webView2);
+        webView1.setWebViewClient(new WebViewClient());
+        webView2.setWebViewClient(new WebViewClient());
+        webView1.loadUrl("https://www.yodobashi.com/");
+        webView2.loadUrl("https://www.amazon.co.jp/");
     }
 }
